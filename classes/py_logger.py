6e34d8a2b4a4ec8ef66e41c2f5caecc6878522b2
zacsa <zac@sanson.co.nz>
2022-04-01 22:32:13 +1300

    Added time based logging

--- classes/py_logger.py
+++ classes/py_logger.py
@@ -3,10 +3,12 @@ Contains all functions required to setup logging
 """
 
 import configparser
+from datetime import datetime
 import logging
 import os
 from logging import Logger
-from logging.handlers import RotatingFileHandler
+from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
+import re
 
 from config.consts import CONFIG_FILENAME
 
@@ -27,6 +29,7 @@ class LoggingTools:
         self.file_format = None
         self.date_format = None
         self.is_file_logging = None
+        self.log_rotation = None
         self.file_location = None
         self.file_path = None
         self.max_file_bytes = None
@@ -34,8 +37,10 @@ class LoggingTools:
         self._read_configs(config_name)
 
         self._create_stdout_logger(logger=logger)
-        if self.is_file_logging:
+        if self.is_file_logging and self.log_rotation == "size_based":
             self._create_rotating_file_logger(logger=logger)
+        elif self.is_file_logging and self.log_rotation == "time_based":
+            self._create_timed_rotating_file_logger(logger=logger)
 
     def _read_configs(self, config_name: str) -> None:
         """
@@ -61,6 +66,8 @@ class LoggingTools:
                 raise MissingConfigurationError("Failed to read basic logger configs")
 
             if self.is_file_logging:
+                self.log_rotation = config_p.get(config_name, "log_rotation")
+
                 self.file_location = config_p.get(config_name, "file_location")
                 file_name = config_p.get(config_name, "file_name")
                 self.file_path = self.file_location + file_name
@@ -112,6 +119,36 @@ class LoggingTools:
         )
         rotating_handler.setFormatter(log_formatter)
         logger.addHandler(rotating_handler)
+        logging.info(f"Created rotating file logger at {self.file_path}")
+
+    def _create_timed_rotating_file_logger(self, logger: Logger) -> None:
+        """
+        Creates a rotating file logger which limits log files size
+        and when exceeding that size, creates a new log file
+        """
+        if not os.path.exists(self.file_location):
+            os.makedirs(self.file_location)
+        rotating_time_handler = TimedRotatingFileHandler(
+            filename=self.file_path,
+            when="midnight",
+            backupCount=self.max_file_no,
+        )
+
+        # def my_namer(default_name):
+        #     # This will be called when doing the log rotation
+        #     # default_name is the default filename that would be assigned, e.g. Rotate_Test.txt.YYYY-MM-DD
+        #     # Do any manipulations to that name here, for example this changes the name to Rotate_Test.YYYY-MM-DD.txt
+        #     base_filename, ext, date = default_name.split(".")
+        #     return f"{base_filename}.{date}.{ext}"
+        # rotating_time_handler.namer = my_namer
+
+        rotating_time_handler.suffix = "%Y-%m-%d"
+        rotating_time_handler.setLevel(self.debug_level)
+        log_formatter = logging.Formatter(
+            fmt=self.file_format, datefmt=self.date_format
+        )
+        rotating_time_handler.setFormatter(log_formatter)
+        logger.addHandler(rotating_time_handler)
         logging.info(f"Created rotating file logger at {self.file_path}")
 
 
