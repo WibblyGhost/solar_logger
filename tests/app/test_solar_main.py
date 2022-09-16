01afbcb367344fe6d000eb0dd38f1011df95bb60
zacsa <zac@sanson.co.nz>
2022-09-17 09:28:20 +1200

    Applied formatting

--- tests/app/test_solar_main.py
+++ tests/app/test_solar_main.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 import pytest
 
+
 @pytest.mark.skip("Solar Logger testing not implemented yet")
 def test_todo():
     raise NotImplementedError
