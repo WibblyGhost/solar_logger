150fd45ee7972887fbb6c29140b0904097e937d9
zacsa <zac@sanson.co.nz>
2022-09-17 09:26:10 +1200

    Add conditional to tag events

--- tests/app/test_influx_query.py
+++ tests/app/test_influx_query.py
@@ -1,3 +1,6 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
+import pytest
+
+@pytest.mark.skip("Influx Query testing not implemented yet")
 def test_todo():
     raise NotImplementedError
--- tests/app/test_solar_main.py
+++ tests/app/test_solar_main.py
@@ -1,3 +1,6 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
+import pytest
+
+@pytest.mark.skip("Solar Logger testing not implemented yet")
 def test_todo():
     raise NotImplementedError
